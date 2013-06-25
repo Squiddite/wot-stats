c3fed33f1e6091127f268f88162b922f5fdfb83a
squiddite <steve@squiddite.net>
2013-06-24 22:09:40 -0400

    remove database schema from php

--- stats.php
+++ stats.php
@@ -1,31 +1,4 @@
 <?
-/*
-http://api.worldoftanks.com/community/accounts/1002993911/api/1.9/?source_token=WG-WoT_Assistant-1.4.1
-
-create table if not exists wotstats (
-   id             int not null auto_increment primary key,
-   playerid       int not null,
-   cachedate      int,
-   statsdate      int,
-   battles        int,
-   victories      int,
-   detections     int,
-   defense        int,
-   kills          int,
-   damage         int,
-   checkpoint     bit not null default 0
-);
-
-create table if not exists wotstats_tanks (
-   statid         int not null,
-   tankname       varchar(100) not null,
-   tier           int,
-   battles        int,
-   victories      int
-);
-alter table wotstats_tanks add foreign key (statid) references wotstats (id) on delete cascade;
-*/
-
    require( "dbConn.php" );
    $dbConnection = new dbConn();
    $mysqli = $dbConnection->getDatabaseConnection();
